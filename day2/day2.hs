3e9a46e0076a7b5d87ed796e3b3cd6f1819eecd3
Konstantinos Metaxas <kmetaxas@gmail.com>
2023-02-26 14:59:06 +0100

    add day2-part 2

--- day2/day2.hs
+++ day2/day2.hs
@@ -17,6 +17,7 @@ normalizeToken x
   | x == 'A' || x == 'X' = 'R'
   | x == 'B' || x == 'Y' = 'P'
   | x == 'C' || x == 'Z' = 'S'
+  | x == 'R' || x == 'P' || x == 'S' = x -- already normalized...
   | otherwise = error (printf"Not a known token for Rockpapersiscorrs=%s"
                       x) 
 -- Calculate the game score based on the following scoring rules:
@@ -42,7 +43,28 @@ choiceScore x
   | x == 'P' = 2
   | x == 'S' = 3
   | otherwise = error (printf "Not known choice %c" x)
-  
+
+-- For a pair of Chars, return the Char to play to get the desired outcome.
+-- X = Loose, Y = draw, Z = win
+getPart2Hand :: Char -> Char -> Char
+getPart2Hand x y
+  | y == 'X' = getLosingForHand x
+  | y == 'Y' = normalizeToken x
+  | y == 'Z' = getWinningForHand x
+  | otherwise = error "Trololo"
+
+getLosingForHand x
+  | n == 'R' = 'S'
+  | n == 'S' = 'P'
+  | n == 'P' = 'R'
+  where n = normalizeToken x
+
+getWinningForHand x
+  | n == 'S' = 'R'
+  | n == 'P' = 'S'
+  | n == 'R' = 'P'
+  where n = normalizeToken x
+
 main = do
     contents <- readFile "input.txt" 
     --let content = [splitAt 1 x | x <- (lines contents)]
@@ -50,4 +72,8 @@ main = do
     let winlose = [ gameResult (fst x) (snd x) | x <- content ]
     let scores =[ gameScore (fst x) (snd x) | x <- content ]
     let totalScore = sum scores
-    printf "Total score = %d" totalScore
+    printf "Total score = %d\n" totalScore
+
+    let part2hands = [ ( (fst x) , (getPart2Hand (fst x) (snd x))) | x <- content ]
+    let scoresPart2 =[ gameScore (fst x) (snd x) | x <- part2hands ] 
+    printf "Part2 score = %d\n" (sum scoresPart2)
